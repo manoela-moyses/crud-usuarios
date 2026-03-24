bebd8698c13fc31fc6fd41637c9a8c53439d73ba
manoela-moyses <manoela.moyses0205@gmail.com>
2026-03-23 23:05:13 -0300

    Seção 7: Criação da tela Home + Configuração de rota

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonComponent } from './components/button/button.component';
+import { LoginComponent } from './pages/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './components/menu/menu.component';
+
+// ANGULAR MATERIAL
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent,
+    LoginComponent,
+    HomeComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyBT1nYziNaX1VFqh5LY_fZrALIr-sKji7c',
+    authDomain: 'crud-usuarios-angular-e8539.firebaseapp.com',
+    projectId: 'crud-usuarios-angular-e8539',
+    storageBucket: 'crud-usuarios-angular-e8539.firebasestorage.app',
+    messagingSenderId: '1046993283065',
+    appId: '1:1046993283065:web:023b94c2904b0e8e177f28',
+  },
+};
